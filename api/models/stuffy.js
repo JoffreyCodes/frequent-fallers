c52bb1783b419d97a44ceceed1875d23cf9b9379
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-19 18:54:37 -0700

    implemnted schema

--- api/models/stuffy.js
+++ api/models/stuffy.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    userName: String,
-    userPassword: String,
-    userSignedIn: Boolean
+const stuffySchema = new Schema({
+  stuffyName: String,
+  primaryColor: String,
+  secondaryColor: String
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Stuffy', stuffySchema);
